8bce40977d78f774efa26844ddd37e4d61eed34c
Florian <florianpaulschoene@gmail.com>
2023-03-09 10:40:00 +0100

    add localStorage

--- src/App.js
+++ src/App.js
@@ -8,31 +8,37 @@ import Skills from "./components/Skills";
 import Form from "./components/Form";
 
 function App() {
-	const [user, setUser] = useState({
-		name: "Fake Dev",
-		job: "Awesome Job",
-		tel: "0800 414141",
-		town: "Jena",
-		country: "Germany",
-		email: "hireme@please.com",
-		git: "github",
-		linkedin: "linkedin",
-		info: "Ad ante tincidunt massa arcu lacus netus maximus habitant nullam convallis augue fermentum venenatis aliquam finibus, iaculis curae eros senectus fusce non ac vehicula natoque congue nunc curabitur himenaeos euismod. Vitae pretium etiam rhoncus aliquet quis per est vel felis id vestibulum, ornare sociosqu praesent inceptos aptent suspendisse varius phasellus tempor dapibus.",
-		skills: {
-			languages: "Javascript, React, HTML, CSS",
-			communication: "German(native), English(fluent)",
-		},
-		projects: {
-			title: "Project1",
-			tech: "Next.js",
-			description:
-				"Adipiscing et lacus nasdasdasdasdadad ad asd asd asdasdasda sdasfws fherastjh awefghqwe rhas HGEARTH  aenean blandit id aliquet mauris nullam cubilia, efficitur class arcu phasellus non sit curae velit ullamcorper nisl ante.",
-		},
-		education: {
-			location: "Fakecampus",
-			level: "Fakeeducation",
-		},
-	});
+	const [user, setUser] = useState(
+		JSON.parse(localStorage.getItem("userInfo")) || {
+			name: "Fake Dev",
+			job: "Awesome Job",
+			tel: "0800 414141",
+			town: "Jena",
+			country: "Germany",
+			email: "hireme@please.com",
+			git: "github",
+			linkedin: "linkedin",
+			info: "Ad ante tincidunt massa arcu lacus netus maximus habitant nullam convallis augue fermentum venenatis aliquam finibus, iaculis curae eros senectus fusce non ac vehicula natoque congue nunc curabitur himenaeos euismod. Vitae pretium etiam rhoncus aliquet quis per est vel felis id vestibulum, ornare sociosqu praesent inceptos aptent suspendisse varius phasellus tempor dapibus.",
+			skills: {
+				languages: "Javascript, React, HTML, CSS",
+				communication: "German(native), English(fluent)",
+			},
+			projects: {
+				title: "Project1",
+				tech: "Next.js",
+				description:
+					"Adipiscing et lacus nasdasdasdasdadad ad asd asd asdasdasda sdasfws fherastjh awefghqwe rhas HGEARTH  aenean blandit id aliquet mauris nullam cubilia, efficitur class arcu phasellus non sit curae velit ullamcorper nisl ante.",
+			},
+			education: {
+				location: "Fakecampus",
+				level: "Fakeeducation",
+			},
+		}
+	);
+
+	useEffect(() => {
+		localStorage.setItem("userInfo", JSON.stringify(user));
+	}, [user]);
 
 	function handleChange(event) {
 		const { name, value } = event.target;
